937940dd805ff0ac3f9a60c57f46f35f2f0a10b1
Lewis Boon <lewis.a.boon@gmail.com>
2020-12-21 22:18:40 +0000

    Added colors and improved if statements

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -2,7 +2,22 @@
 
 set -e
 
-echo "Setting up environment..."
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# ====================================================
+
+echo "${Blue}Setting up environment...${Color_Off}"
 echo ""
 
 echo "Installing zsh"
@@ -10,21 +25,31 @@ sudo apt update
 sudo apt install zsh -y
 
 # Install Oh my ZSH
-echo "Installing oh-my-zsh"
-if [ -d ~/.oh-my-zsh ]; then
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "${Yellow}Installing oh-my-zsh.${Color_Off}"
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo "${Green}oh-my-zsh already installed.${Color_Off}"
+else
+  echo "${Green}oh-my-zsh is already installed.${Color_Off}"
 fi
 
 # Install Powerline 10k
-echo "Installing Powerline 10k"
-if [ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
+if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
+  echo "${Yellow}Installing Powerline 10k${Color_Off}"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  echo "${Green}Powerline 10k installed successfully.${Color_Off}"
+else
+  echo "${Green}Powerline 10k already installed.${Color_Off}"
 fi
 
 # Setup dotfiles
-echo "Cloning dotfiles repo"
-if [ -d ~/.dotfiles ]; then
+
+if [ ! -d ~/.dotfiles ]; then
+  echo "${Yellow}Cloning dotfiles repo.${Color_Off}"
   git clone https://github.com/labooner/dotfiles ~/.dotfiles
+  echo "${Green}dotfiles successfully cloned.${Color_Off}"
+else
+  echo "{Green}dotfiles already exists.${Color_Off}"
 fi
 
 echo "Create symlinks for dot files"
