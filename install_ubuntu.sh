065355af03bcb65006772797a303c4b16985fe8c
Lewis Boon <lewis.a.boon@gmail.com>
2020-12-21 21:49:24 +0000

    Adding debug lines to install script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,25 +3,31 @@
 set -e
 
 echo "Setting up environment..."
+echo ""
 
+echo "Installing zsh"
 sudo apt update
 sudo apt install zsh -y
 
 # Install Oh my ZSH
+echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Powerline 10k
+echo "Installing Powerline 10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Setup dotfiles
+echo "Cloning dotfiles repo"
 git clone https://github.com/labooner/dotfiles ~/.dotfiles
 
+echo "Create symlinks for dot files"
 ln -sfn ~/.dotfiles/.vimrc ~/.vimrc
 ln -sfn ~/.dotfiles/.gitignore ~/.gitignore
 ln -sfn ~/.dotfiles/.zshrc ~/.zshrc
 
 # Setup SSH keys
-echo "Generate an SSH keypair: ssh -t ed25519 -C "labooner.github.com"
+echo "Generate an SSH keypair: ssh -t ed25519 -C \"labooner.github.com\""
 
 # Setup GPG keys
 echo "Generate a GPG key: gpg --full-generate-key"
